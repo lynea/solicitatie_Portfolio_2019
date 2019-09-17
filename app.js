103127aa28b165b88442d9c36b38ad6886f68626
Rene van Dijk <renevandijk3@hotmail.com>
2019-09-17 10:37:47 +0200

    split code into reusable components

--- app.js
+++ app.js
@@ -11,4 +11,17 @@ app.get("/", function(req, res) {
     res.render('index')
 });
 
+
+// project routes
+
+app.get("/projects", function(req, res) {
+    res.render('projects')
+});
+
+app.get("/projects/elderly", function(req, res) {
+    res.render('./projects/elderly')
+});
+
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
