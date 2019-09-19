89939e3499b755e29da9a3d6ae3157c2ca53c8b3
Rene van Dijk <renevandijk3@hotmail.com>
2019-09-19 16:57:04 +0200

    add active indicator menu - adjust animations

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,8 +5,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
     let name = document.querySelector(".navbar-brand");
     let nav = document.querySelector("nav");
-   
+   let navLinks = document.querySelectorAll(".nav li"); 
 
+   navLinks.forEach(function(navItem){
+     navItem.addEventListener('click', function(){
+      navLinks.forEach(function(navItem){
+        navItem.classList.remove("active-menu-item")
+      })
+      this.classList.add("active-menu-item")
+       
+     })
+   })
     
   window.onscroll = () => {
     var y = window.scrollY; 
@@ -15,9 +24,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     if (y <= 40){
     name.textContent = 'Rene van Dijk'
     name.classList.remove("scroll-active");
+    name.classList.add("brand-regular")
  
     }else{
         name.classList.add("scroll-active")
+        name.classList.remove("brand-regular");
         name.textContent = 'R.'
       
     }
@@ -38,12 +49,32 @@ document.addEventListener("DOMContentLoaded", function(event) {
   };
 
   ScrollReveal({ distance: '60px' });
-  ScrollReveal().reveal('.timeline-panel', { delay: 600, interval: 700, origin: 'left', scale: 0.85 });
-  
-  ScrollReveal().reveal('.portfolio-image-big', { delay: 600 });
-  ScrollReveal().reveal('.project-preview-img', { delay: 600 });
-  ScrollReveal().reveal('.summary-container', { delay: 600 });
-  ScrollReveal().reveal('.subText-Container', { delay: 1100 });
 
+  ScrollReveal().reveal('.timeline-panel', {  interval: 700, origin: 'left', scale: 0.85 });
+
+  ScrollReveal().reveal('.bigTextBlack', { delay: 200, origin: 'left', scale: 0.85, duration: 1000 });
+  
+  // red text
+  ScrollReveal().reveal('.animation-one', { delay: 1000,  origin: 'left', scale: 0.85, duration: 1000 });
+  ScrollReveal().reveal('.animation-two', { delay: 1500,  origin: 'left', scale: 0.85, duration: 1000 });
+  ScrollReveal().reveal('.animation-three', { delay: 2000,  origin: 'left', scale: 0.85, duration: 1000 });
+// image header
+  ScrollReveal().reveal('.header-img', { delay: 600,  origin: 'right', scale: 0.85, duration: 1000 });
+
+  // portfolio animations
+  ScrollReveal().reveal('.portfolio-image-big', { interval: 700, origin: 'right', scale: 0.85, duration: 900 });
+  ScrollReveal().reveal('.project-preview-img', {  interval: 700, origin: 'right', scale: 0.85, duration: 900 });
+  ScrollReveal().reveal('.huge-img', { delay: 400 ,opacity: 0, duration: 1000 });
+  ScrollReveal().reveal('.summary-container', {  origin: 'right', scale: 0.85, duration: 900  });
+  ScrollReveal().reveal('.subText-Container', {   interval: 700, origin: 'right', scale: 0.85, duration: 900 });
+
+
+
+  // portfolio header 
+ 
+
+  ScrollReveal().reveal('.project-title', { delay: 500, origin: 'right', scale: 0.65, duration: 900 });
+  ScrollReveal().reveal('.project-subtitle', { delay: 1000, origin: 'right', scale: 0.65, duration: 900 });
+  
   });
 
