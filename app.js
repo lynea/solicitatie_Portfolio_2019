04cb93a7a4b299aa1449e1e1d8e60d9fc361e369
Rene van Dijk <renevandijk3@hotmail.com>
2019-09-20 13:02:49 +0200

    some mobile adjustments

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.get("/elderly", function(req, res) {
 
 
 
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.listen(process.env.PORT, process.env.IP);
+// app.listen(process.env.PORT, process.env.IP);
