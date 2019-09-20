c28e751482f1a10a54460a4446957b53376ec167
Rene van Dijk <renevandijk3@hotmail.com>
2019-09-20 11:33:24 +0200

    correct app.listen

--- app.js
+++ app.js
@@ -24,4 +24,6 @@ app.get("/elderly", function(req, res) {
 
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.listen(process.env.PORT, process.env.IP)
